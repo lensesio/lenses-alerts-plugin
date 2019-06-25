8aec9b89a8aed0fd26cd1e478d6bf3a8941d1a1b
Stephen Kennedy <stephen@thekennedy.co>
2019-06-25 13:24:43 +0100

    Add override without timestamp

--- lenses-alerts-scala/src/main/scala/io/lenses/alerting/plugin/scalaapi/Alert.scala
+++ lenses-alerts-scala/src/main/scala/io/lenses/alerting/plugin/scalaapi/Alert.scala
@@ -31,3 +31,15 @@ case class Alert(level: AlertLevel,
       map.asJava)
   }
 }
+
+object Alert {
+  def apply(level: AlertLevel,
+            category: String,
+            tags: List[String],
+            instance: String,
+            summary: String,
+            docs: Option[String],
+            alertId: Int,
+            map: Map[String, String]): Alert =
+    apply(level, category, tags, instance, summary, docs, System.currentTimeMillis(), alertId, map)
+}
