599b701c2b680dd8417e1186406adffb44cf9fb7
stefan bocutiu <stefan.bocutiu@gmail.com>
2019-07-07 23:15:49 +0100

    Fix the test compilation

--- lenses-alerts-scala/src/test/scala/io/lenses/alerting/plugin/scalaapi/BasePlugin.scala
+++ lenses-alerts-scala/src/test/scala/io/lenses/alerting/plugin/scalaapi/BasePlugin.scala
@@ -3,13 +3,13 @@ package io.lenses.alerting.plugin.scalaapi
 import io.lenses.alerting.plugin.javaapi.{AlertingPlugin => JAlertingPlugin}
 import io.lenses.alerting.plugin.javaapi
 import io.lenses.alerting.plugin.javaapi.util.Try
-import io.lenses.alerting.plugin.javaapi.ConfigEntry
+import io.lenses.alerting.plugin.javaapi.{ConfigEntry=>JConfigEntry}
 
 import scala.collection.JavaConverters._
 
 class TestBasePlugin(thunk: => Try[javaapi.AlertingService]) extends JAlertingPlugin {
   override def name(): String = "name"
   override def description(): String = "description"
-  override def configKeys(): java.util.List[ConfigEntry] = List.empty.asJava
+  override def configKeys(): java.util.List[JConfigEntry] = List.empty.asJava
   override def init(config: java.util.Map[String, String]): Try[javaapi.AlertingService] = thunk
 }
